cc42355a2409d09ebf080d5a4e6775eb280bf51f
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-11 17:30:21 +0000

    edited go

--- go_app/main.go
+++ go_app/main.go
@@ -25,6 +25,7 @@ func main() {
 	fmt.Println()
 	fmt.Print("Enter your third side length: ")
 	fmt.Scanln(&sideC)
+	fmt.Println()
 
 	// process
 	// cosine law
@@ -32,10 +33,32 @@ func main() {
 	angleB := (sideA * sideA) + (sideC * sideC) - (sideB * sideB) / (2 * sideA * sideC) * (180 / math.Pi)
 	angleC := (sideA * sideA) + (sideB * sideB) - (sideC * sideC) / (2 * sideA * sideB) * (180 / math.Pi)
 
+	angleAFormatted := fmt.Sprintf("%.2f", angleA)
+	angleBFormatted := fmt.Sprintf("%.2f", angleB)
+	angleCFormatted := fmt.Sprintf("%.2f", angleC)
 
+	sumOfAngles := angleAFormatted + angleBFormatted + angleCFormatted
 
-
-
+	if sumOfAngles == "180" {
+		if angleAFormatted != angleBFormatted && angleAFormatted != angleCFormatted && angleBFormatted != angleCFormatted {
+			if angleAFormatted == 90 || angleBFormatted == 90 || angleCFormatted == 90{
+				fmt.Println("You have a right angle triangle")
+			} else {
+				fmt.Println("You have a scalene triangle")
+			}
+		} else {
+			if angleAFormatted == angleBFormatted && angleAFormatted == angleCFormatted {
+				fmt.Println("You have an equilateral triangle")
+			} else {
+				if angleAFormatted == 90 || angleBFormatted == 90 || angleCFormatted == 90{
+					fmt.Println("You have a right angle triangle")
+				} else {
+					fmt.Println("You have an isosceles triangle")
+			}
+		}
+	} else {
+		fmt.Println("Please enter a valid triangle")
+	}
 
 	fmt.Println()
 	fmt.Println("\nDone.")
